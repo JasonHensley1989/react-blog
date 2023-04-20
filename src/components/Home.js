39517bc1d97594d25c4cfc02eb7e8d850cccc779
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-20 16:34:52 -0500

    react state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
 const Home = () => {
+    let name = "Jason";
+
     const handleClick = () => {
-        console.log(`Hello`);
+        name = 'Luigi';
+        console.log(name);
     }
 
-    const handleClickAgain = (name) => {
-        console.log(`Hello ${name} great to see you again`);
-    }
     return (
         <div className='home'>
             <h2>Homepage</h2>
+            <p>{ name }</p>
             <button onClick={handleClick}>Click</button>
-            <button onClick={() => handleClickAgain("Jason")}>Click Again</button>
-            {/* <h2>Create New Blog</h2> */}
+          
         </div>
   )
 }
