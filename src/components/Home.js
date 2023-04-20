404060420a42ea19166b571ef5f4b1d205d4372e
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-20 16:41:33 -0500

    state and hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,19 @@
+import { useState } from 'react';
 import React from 'react'
 
 const Home = () => {
-    let name = "Jason";
+    const [name, setName] = useState("Jason");
+    const [age, setAge] = useState(25)
 
     const handleClick = () => {
-        name = 'Luigi';
-        console.log(name);
+        setName("Jason Hensley");
+        setAge(Math.round(Math.random() * 100));
     }
 
     return (
         <div className='home'>
             <h2>Homepage</h2>
-            <p>{ name }</p>
+            <p>Hello { name } is { age } years old!</p>
             <button onClick={handleClick}>Click</button>
           
         </div>
