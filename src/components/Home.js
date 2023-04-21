8cd1fddb75d15c3e379e885cc14852cdb010beaa
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-21 12:28:58 -0500

    outputting lists

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,10 @@ import { useState } from 'react';
 import React from 'react'
 
 const Home = () => {
-    const [name, setName] = useState("Jason");
-    const [age, setAge] = useState(25)
-
-    const handleClick = () => {
-        setName("Jason Hensley");
-        setAge(Math.round(Math.random() * 100));
-    }
 
     return (
         <div className='home'>
             <h2>Homepage</h2>
-            <p>Hello { name } is { age } years old!</p>
-            <button onClick={handleClick}>Click</button>
         </div>
   )
 }
