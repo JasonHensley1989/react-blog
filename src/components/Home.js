f32b98cc627440e2b751d6d59df93879b0c85180
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-21 12:31:15 -0500

    setting state with lists

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,11 @@ import { useState } from 'react';
 import React from 'react'
 
 const Home = () => {
-
+    const [blogs, setBlogs] = useState([
+        { title: 'My new blog site', body: 'Lorem impsum....', author: "Jason", id: 1 },
+        { title: 'My new blog site', body: 'Lorem impsum different....', author: "Jason", id: 2 },
+        { title: 'My new blog site', body: 'Lorem impsum.... lastly', author: "Jason", id: 3 }
+    ])
     return (
         <div className='home'>
             <h2>Homepage</h2>
