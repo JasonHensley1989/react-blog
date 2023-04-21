eab5584ba41c94d906e18245e246a487dd33f298
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-21 12:42:07 -0500

    lists of articles/blog css styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,19 @@ import React from 'react'
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new blog site', body: 'Lorem impsum....', author: "Jason", id: 1 },
-        { title: 'My new blog site', body: 'Lorem impsum different....', author: "Jason", id: 2 },
-        { title: 'My new blog site', body: 'Lorem impsum.... lastly', author: "Jason", id: 3 }
-    ])
+        { title: 'A new day', body: 'Lorem impsum different....', author: "Jason", id: 2 },
+        { title: 'Got something done ', body: 'Lorem impsum.... lastly', author: "Jason", id: 3 }
+    ]);
+
     return (
         <div className='home'>
-            <h2>Homepage</h2>
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>{ blog.body }</p>
+                    <p>{ blog.author}</p>
+                </div>
+            ))}
         </div>
   )
 }
