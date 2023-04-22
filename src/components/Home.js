de337ab10ef639e02a2cc080f305d6bece68681f
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-22 13:38:59 -0500

    use effect changes based on scope

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ const Home = () => {
         { title: 'Got something done ', body: 'Lorem impsum.... lastly', author: "Jason", id: 3 }
     ]);
     
+    const [name, setName] = useState("Jason");
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -17,11 +18,14 @@ const Home = () => {
 
     useEffect(() => {
         console.log("Use effect ran");
-    });
+        console.log(name);
+    }, [name]);
 
     return (
         <div className='home'>
            <Bloglist blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+           <button onClick={() => setName("Jason H.")}>Change Name</button>
+           <p>{ name }</p>
         </div>
   )
 }
