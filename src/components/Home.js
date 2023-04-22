e37c5b72ded8ef7a75a54ded437c40f3b723e306
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-22 12:01:38 -0500

    reusing components

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,14 @@ import Bloglist from './Bloglist';
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new blog site', body: 'Lorem impsum....', author: "Jason", id: 1 },
-        { title: 'A new day', body: 'Lorem impsum different....', author: "Jason", id: 2 },
+        { title: 'A new day', body: 'Lorem impsum different....', author: "Fred", id: 2 },
         { title: 'Got something done ', body: 'Lorem impsum.... lastly', author: "Jason", id: 3 }
     ]);
 
     return (
         <div className='home'>
            <Bloglist blogs={blogs} title="All Blogs"/>
+           <Bloglist blogs={blogs.filter((blog) => blog.author === "Jason")} title="Jasons Blogs"/>
         </div>
   )
 }
