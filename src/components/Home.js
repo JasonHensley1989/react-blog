1c6733f7ff620e50a15a15c1cf12b7116db2ceb6
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-23 14:53:08 -0500

    conditional loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,8 @@ import Bloglist from './Bloglist';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
     
-    const [name, setName] = useState("Jason");
-
-    // const handleDelete = (id) => {
-    //     const newBlogs = blogs.filter(blog => blog.id !== id);
-    //     setBlogs(newBlogs);
-    // }
-
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
             .then(res => {
@@ -19,15 +13,17 @@ const Home = () => {
             })
             .then(data => {
                 console.log(data);
-                setBlogs(data)
+                setBlogs(data);
+                setIsPending(false)
             });
     }, []);
 
+
     return (
         <div className='home'>
+            {isPending && <div>Loading...</div>}
            {blogs && <Bloglist blogs={blogs} title="All Blogs" />}
            {/* <button onClick={() => setName("Jason H.")}>Change Name</button> */}
-           {/* <p>{ name }</p> */}
         </div>
   )
 }
