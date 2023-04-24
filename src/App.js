90a9e82316447946b996782549d437a75577542a
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-24 16:07:21 -0500

    404 page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Create from "./Create";
 import './index.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import BlogDetails from "./components/BlogDetails";
+import NotFound from "./components/NotFound";
 
 function App() {
   const title = "The Daily Blogger";
@@ -22,6 +23,10 @@ function App() {
           </Route>
           <Route path='/blogs/:id'>
               <BlogDetails />
+          </Route>
+          {/* Goes on bottom else it will match any of the top  */}
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
         </div>
