727774b648f7dbab037c51b5e5bb249d51d00342
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-24 14:29:16 -0500

    add blog component and styling

--- src/Create.js
+++ src/Create.js
@@ -4,6 +4,24 @@ const Create = () => {
   return (
     <div className="create">
         <h2>Add New Blog</h2>
+        <form>
+          <label>Blog Title: </label>
+          <input 
+          type='text' 
+          required 
+          />
+          <label>Blog Body: </label>
+          <textarea
+            required
+          ></textarea>
+          <label>Blog Author: </label>
+          <select>
+            <option value="Jason">Jason</option>
+            <option value="J.A. Hensley">J.A. Hensley</option>
+            <option value="J.H.">J.H.</option>
+          </select>
+          <button>Add Blog</button>
+        </form>
     </div>
   )
 }
