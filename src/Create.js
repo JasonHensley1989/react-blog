1b490e14a7d07f027156a91df723c8ad72e143af
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-24 14:39:48 -0500

    blog tracking

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
   return (
     <div className="create">
         <h2>Add New Blog</h2>
@@ -9,18 +13,28 @@ const Create = () => {
           <input 
           type='text' 
           required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
           />
           <label>Blog Body: </label>
           <textarea
             required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
           ></textarea>
           <label>Blog Author: </label>
-          <select>
+          <select
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          >
             <option value="Jason">Jason</option>
             <option value="J.A. Hensley">J.A. Hensley</option>
             <option value="J.H.">J.H.</option>
           </select>
           <button>Add Blog</button>
+          <p>{ title }</p>
+          <p>{ body }</p>
+          <p>{ author }</p>
         </form>
     </div>
   )
