cceae61f12f368c9b7315b4fbf7f2e6d705b42b6
Jason Hensley <jasonhensley1989@gmail.com>
2023-04-24 15:59:00 -0500

    db fix

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+  const history = useHistory();
+
+  const handleClick = (e) => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push('/');
+    })
+  }
 
   return (
     <div className='blog-details'>
@@ -14,7 +23,8 @@ const BlogDetails = () => {
           <article>
             <h2>{ blog.title }</h2>
             <p>Written by: { blog.author }</p>
-            <div>{ blog.body }</div>
+            <div required>{ blog.body }</div>
+            <button onClick={handleClick}>Delete</button>
           </article>
         )}
     </div>
